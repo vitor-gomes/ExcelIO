53089946a894abd2b2764aab1813fb8e4934b73e
pedrogonic <pedrogonic@gmail.com>
2018-12-27 18:59:04 -0200

    Creating custom annotation for ExcelColumn.
    This Annotation should be use for importing and exporting. Annotate fields with their name, index (in case of importing) and columnDefinition (for exporting date columns).
    This reverses the use of the JPA Column Annotation to prevent collisions with JPA Model classes.

---
+++ src/main/java/com/io/excel/annotations/ExcelColumn.java
@@ -0,0 +1,22 @@
+package com.io.excel.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author pcoelho
+ */
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+public @interface ExcelColumn {
+    String name() default "";
+    String columnDefinition() default "";
+    String index() default "";
+}
